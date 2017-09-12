1b34d99a4996a4524c24a3d8969d3a1486782e9e
Nicholas Smith <npsmith1990@gmail.com>
2017-09-12 15:29:46 -0500

    Initial Commit

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+
+	public LevelManager levelManager;
+	void OnTriggerEnter2D(Collider2D trigger) {
+		levelManager.LoadLevel("Win");
+	}
+	
+	void OnCollisionEnter2D(Collision2D collision) {
+	}
+}
